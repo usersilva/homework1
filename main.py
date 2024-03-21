71676033e90cef19564c6c25701338ff051546ff
usersilva <silvianelson2003@gmail.com>
2024-03-21 23:23:51 +0300

    my first pyHomework

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+def func(num):
+    maxi = 0
+    while num > 0:
+        current = num % 10
+        if current > maxi:
+            maxi = current
+        num //= 10
+    print("max value:", maxi)
 
+
+num = int(input("enter the number "))
+func(num)
